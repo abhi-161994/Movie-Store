96fe7ccbbe1b67ce4e237bf0530e7f0d7747c5d4
abhishek <abhishek.nanda@maropost.com>
2017-12-12 13:39:58 +0000

    implement _form page and new and create logic

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,45 @@
+class MoviesController < ApplicationController
+
+  before_action :set_id,except:[:new,:create,:index]
+
+  def index
+    @movies = Movie.new
+  end
+
+  def new
+    @movies = Movie.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @movies = Movie.new(movie_params)
+    if @movies.save
+      redirect_to @movies,notice: "Movie Successfully Saved"
+    else
+      render 'new'
+  end
+end
+
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title,:image,:genre,:plot,:rating,:web)
+   end
+
+
+  def set_id #this is for call backs
+    @movies = Movie.find(params[:id])
+  end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ApplicationRecord
+  validates_presence_of :title,:genre,:plot,:rating,:web
 end
