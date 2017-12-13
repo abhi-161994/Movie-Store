f5ec2d99364bd07c0e058d4f9b7f97a42c1f2764
abhishek <abhishek.nanda@maropost.com>
2017-12-13 10:03:56 +0000

    Add yrs to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212115629) do
+ActiveRecord::Schema.define(version: 20171213100153) do
 
   create_table "movies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171212115629) do
     t.string "web"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "year"
   end
 
 end
