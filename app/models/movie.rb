6c997055fd938c6b361d3bceeb59515865188084
abhishek <abhishek.nanda@maropost.com>
2017-12-13 11:32:38 +0000

    setup th show page

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
   mount_uploader :image,MovieUploader
-  validates_presence_of :title,:genre,:plot,:rating,:web
+  validates_presence_of :title,:genre,:plot,:rating,:web,:year
 end
