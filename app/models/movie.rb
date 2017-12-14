76b96cbd5371ba66bbe187887c2db0b6a62b9241
abhishek <abhishek.nanda@maropost.com>
2017-12-14 07:40:55 +0000

    add validation of rating some stylesheets

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
   mount_uploader :image,MovieUploader
-  validates_presence_of :title,:genre,:plot,:rating,:web,:year
+  validates_presence_of :title,:genre,:plot,:rating,:web,:year,:cast
+  validates :rating, numericality: {less_than_or_equal_to: 10}
 end
