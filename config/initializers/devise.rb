091325f23a1ad582b2ff0a2b8416c96f67eb6a42
abhi-161994 <abhishek.nanda@maropost.com>
2017-12-27 02:51:38 +0530

    done Omni auth for fb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,7 +250,7 @@ Devise.setup do |config|
   # == OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, "152823678696693", "10ee39caedc7881e3626117d2aa6c179"
+  config.omniauth :facebook, "152823678696693", "10ee39caedc7881e3626117d2aa6c179", scope: 'email', info_fields: 'email,name'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   match '/admin/movies' => 'movies#create', via: :post
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users, controller: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'movies/detail' => 'movies#detail',as: :movies_detail
   resources :movies,:except => [:new,:delete,:edit,:update] do
     resources :comments
   end
-  root to: "movies#index"
-
+    root to: "movies#index"
+  #root to: "home#index"
 #   devise_scope :user do
 #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
 # end
