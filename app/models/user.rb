bb95323084194cd514c3723935181119973a6448
abhishek <abhishek.nanda@maropost.com>
2017-12-26 13:42:30 +0000

    Add Session expires in devise and lock account

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable ,:lockable, :timeoutable
          has_many :comments, dependent: :destroy
          has_many :mmovies, through: :comments
   validates_presence_of :name
