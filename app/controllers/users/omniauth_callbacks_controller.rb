594b539c7cb9934d6915609c00ec5cbcde03d2fc
abhi-161994 <abhishek.nanda@maropost.com>
2018-01-08 00:57:48 +0530

    omni-auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,26 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
-      user = User.from_omniauth(request.env["omniauth.auth"])
-      if user.persisted?
-        session[:user_id] = user.id
-        sign_in_and_redirect user, notice: "Signed in!"
-      else
-        session["devise.user_attributes"] = user.attributes
-        redirect_to new_user_registration_url
-      end
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      session[:user_id] = user.id
+      sign_in_and_redirect user, notice: "Signed in!"
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
     end
+  end
 
-    alias_method :facebook, :all
-    alias_method :google_oauth2, :all
-     alias_method :twitter, :all
+  private
+   def omni_helper
+     user = User.from_omniauth(request.env["omniauth.auth"])
+     if user.provider == "facebook"
+       return "Google"
+     elsif user.provider == "google_oauth2"
+       return "Facebook"
+     elsif user.provider == "twitter"
+     return "twitter"
+     end
+   end
+
 end
