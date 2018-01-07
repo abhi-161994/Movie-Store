f382460ac49e990967b00b48de0a6993b66f2bad
abhi-161994 <abhishek.nanda@maropost.com>
2018-01-07 22:44:30 +0530

    djd

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,18 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-   def all
-     @user = User.from_omniauth(request.env["omniauth.auth"])
+  def all
+      user = User.from_omniauth(request.env["omniauth.auth"])
+      if user.persisted?
+        session[:user_id] = user.id
+        sign_in_and_redirect user, notice: "Signed in!"
+      else
+        session["devise.user_attributes"] = user.attributes
+        redirect_to new_user_registration_url
+      end
+    end
 
-     if @user.persisted?
-           sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-           set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-         else
-           session["devise.facebook_data"] = request.env["omniauth.auth"]
-           redirect_to new_user_registration_url
-         end
-       end
-
-
-       alias_method :facebook, :all
-       alias_method :google_oauth2, :all
-       alias_method :twitter, :all
+    alias_method :facebook, :all
+    alias_method :google_oauth2, :all
+     alias_method :twitter, :all
 end
